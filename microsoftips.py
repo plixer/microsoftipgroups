f58cbbcdf6d7e69a744c05047acbadc4f7e20c17
Brian Davenport <briandav22@gmail.com>
2019-12-19 10:05:44 -0500

    first commit

---
+++ microsoftips.py
@@ -0,0 +1,78 @@
+import json
+import requests
+from scrut_api import ReportAPI, Requester
+
+
+#gather data from microsoft, and convert it to JSON. 
+ip_data_raw = requests.get("https://endpoints.office.com/endpoints/Worldwide?ClientRequestId=b10c5ed1-bad1-445f-b386-b919946339a7")
+microsoft_ips = ip_data_raw.json()
+
+print('Microsoft Data Retrieved')
+
+report_params = ReportAPI()
+scrutinizer_requester = Requester(
+    authToken="tzvqXbREMSxN8djtKrTZmnM1",
+    hostname="demo.plixer.com"
+)
+
+
+#simple way of remove all duplicate IP Adddresses from the multiple lists. 
+def remove_duplicates(list_of_items):
+    print('Removing Duplicate IP addresses')
+    for group in list_of_items:
+        list_of_items[group] = set(list_of_items[group])
+    return list_of_items
+
+#function to filter microsoft data for groups we want, and return data we want. 
+def filter_microsoft_data(microsoft_ips):
+
+    groups_to_add = {
+        'Exchange':[],
+        'Skype':[],
+        'SharePoint':[]
+    }
+    print('Filtering Data to Add Desired Groups')
+    for service in microsoft_ips:
+        try:
+            #since we are creating IP groups, we only want the data that contains ips.
+            if service['serviceArea'] in groups_to_add and service['ips']:
+                groups_to_add[service['serviceArea']].extend(service['ips'])
+        except:
+            pass
+
+
+    groups_to_add = remove_duplicates(groups_to_add)      
+    return groups_to_add
+
+#function to use the API to create the group data.
+def use_api_to_create_groups(microsoft_ips, report_params, scrutinizer_requester):
+    
+    for ip_group in microsoft_ips:
+        data_for_api = []
+        for ip_subnet in microsoft_ips[ip_group]:
+            subnet = ip_subnet.split('/')[0]
+            mask = ip_subnet.split('/')[1]
+            data_for_api.append({"type":"network", "address":subnet, "mask":mask})
+        
+        #Do a quick check to convert Skype to include teams as well.
+        try:
+            if ip_group =='Skype':
+                ip_group = 'Skype or Teams'
+        except:
+            pass
+
+        report_params.create_group(ip_group, json.dumps(data_for_api))
+        scrutinizer_requester.make_request(report_params)
+
+
+
+
+microsoft_ips = filter_microsoft_data(microsoft_ips)
+
+use_api_to_create_groups(microsoft_ips, report_params, scrutinizer_requester)
+
+
+
+
+
+
---
+++ scrut_api.py
@@ -0,0 +1,187 @@
+import requests
+import requests.packages.urllib3
+import json
+
+requests.packages.urllib3.disable_warnings()
+
+
+
+class ReportAPI:
+    def __init__(self):
+        
+        self.options = {
+
+            "reportTypeLang": "conversationsApp",
+            "reportDirections": {"selected": "inbound"},
+            "dataGranularity": {"selected": "auto"},
+            "orderBy": "sum_octetdeltacount",
+            "times": {"dateRange": "LastTenMinutes"},
+            "filters": {
+                        "sdfDips_0": "in_GROUP_ALL"
+                    },
+            "rateTotal": {"selected": "rate"},
+            "dataFormat": {"selected": "normal"},
+            "bbp": {"selected": "percent"},
+            "view":"topInterfaces",
+            "unit":"percent"
+        }
+        self.direction = {
+            "inbound": {
+                "graph": "all",
+                "table": {
+                    "query_limit": {
+                        "offset": 0,
+                        "max_num_rows": 10
+                    }
+                }
+            }
+        }
+        self.params = None
+    def report_options(self,
+                    reportTypeLang="conversationsApp",
+                    reportDirections= "inbound",
+                    dataGranularity="auto",
+                    orderBy="sum_octetdeltacount",
+                    times={"dateRange": "LastTenMinutes"},
+                    filters={
+                        "sdfDips_0": "in_GROUP_ALL"
+                    },
+                    rateTotal={"selected": "rate"},
+                    dataFormat={"selected": "normal"},
+                    bbp={"selected": "percent"},
+                    view="topInterfaces",
+                    unit="percent"):
+        self.options = {
+
+            "reportTypeLang": reportTypeLang,
+            "reportDirections": {"selected": reportDirections},
+            "dataGranularity": {"selected": dataGranularity},
+            "orderBy": orderBy,
+            "times": times,
+            "filters": filters,
+            "rateTotal": rateTotal,
+            "dataFormat": dataFormat,
+            "bbp": bbp
+        }
+
+    def create_group(self, group_name, group_rule, group_id = '', action = 'add'):
+        self.params = {
+            "rm":"ipgroups",
+            "name":group_name,
+            "added": group_rule,
+            "apptype":"internal",
+            "action":action,
+            "subCat":'IPGroups',
+            "fc_id": group_id
+
+
+
+        }
+
+    def find_all_groups(self):
+        self.params = {
+            "rm": "ipgroups",
+            "action": "loadTreeRootFast",
+            "subCat": "IPGroups"
+        }
+    
+
+    def report_direction(self, report_direction="inbound", max_rows=10):
+        self.direction = {
+            report_direction: {
+                "graph": "all",
+                "table": {
+                    "query_limit": {
+                        "offset": 0,
+                        "max_num_rows": max_rows
+                    }
+                }
+            }
+        }
+
+
+    def make_object(self):
+        self.params = {
+            "rm":"report_api",
+            "action":"get",
+            "rpt_json":json.dumps(self.options),
+            "data_requested":json.dumps(self.direction)
+        }
+
+
+
+class Requester:
+    def __init__(self, authToken=None, hostname=None):
+        self.authToken = authToken
+        self.hostname = hostname
+
+    def error_checker(self, json):
+        # error handler
+        if 'err' in json:
+            error_msg = json['err']
+            if 'details' in json:
+                error_details = json['details']
+                print("Looks like you API key may not be valid, I received this back\nError: {}\nDetails: {},\nAPI Key: {}".format(
+                    error_msg, error_details, self.authToken))
+                return
+            else:
+                print("Received an Error {}".format(
+                    error_msg))
+                return
+
+    def verify_https(self, response, params):
+        # used so the user doesn't have to worry about the URL to Scrutinizer, only the hostname.
+        try:
+            if response.history[0].status_code == 302:
+                r = requests.get(
+                    "https://{}/fcgi/scrut_fcgi.fcgi?".format(self.hostname), params=params, verify=False)
+                return r
+        except:
+            print("Seems you are running Scrutinizer in HTTP, consider enabling SSL for more secure use of the API")
+            return response
+
+    def intiated_check(self):
+        # checks to make sure the hostname and authtoken were passed
+        if self.hostname == None or self.authToken == None:
+            print("Did not receive either a Hostname or a Authoken when Class was initiated\nHere are the values I recieved: \nHostname:{} \nAuthtoken: {}".format(
+                self.hostname, self.authToken))
+            return True
+
+    def make_request(self, report_object):
+
+        # make sure user passed in hostname and authtoken
+        params = report_object.params
+        if self.intiated_check():
+            return
+        # add authToken to the Params
+        params['authToken'] = self.authToken
+
+        # make request to Scrutinizer
+        data_back = requests.get(
+            "https://{}/fcgi/scrut_fcgi.fcgi?".format(self.hostname), params=params, verify=False)
+
+        # check to see if user is using HTTPS, if they are, make request to HTTPS address.
+        response = self.verify_https(data_back, params)
+        # convert response to JSON.
+        response = response.json()
+
+        # check for any errors in the JSON response
+        if self.error_checker(response):
+            return
+
+        # return the response
+
+        return response
+
+
+
+class scrut_print:
+    '''Most used for error check and seeing the data Scrutinizer is returning. It prints the JSON data out in a nicely formatted way to make it easier to read.'''
+    def __init__(self, data_to_print):
+        self.scrut_class = data_to_print
+        if isinstance(data_to_print, dict):
+            print(json.dumps(data_to_print, indent=4, sort_keys=True))
+        else:
+            for attribute in self.scrut_class.__dict__:
+                print(attribute + ' : ' +
+                      str(self.scrut_class.__dict__[attribute]))
